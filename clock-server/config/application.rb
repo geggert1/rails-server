c1c4f0259e451656cea7ad3de2c33d2b41f17202
geggert1 <gary.eggerton@gmail.com>
2016-03-15 19:28:39 +0000

    Checking push to remote

--- clock-server/config/application.rb
+++ clock-server/config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Ian was ere. So was Gazza!
+# Ian was ere. So was Gazza! Take that! piargh!
 Bundler.require(*Rails.groups)
 
 module ClockServer
